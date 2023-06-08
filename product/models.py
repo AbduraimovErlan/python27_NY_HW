f26c0e762e1fbc432fea65719d323e3386b22bab
AbduraimovErlan <96403251+AbduraimovErlan@users.noreply.github.com>
2023-06-08 13:05:08 -0400

    Initial commit

---
+++ product/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.PositiveIntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='category')
+
+    def __str__(self):
+        return self.title
+
+
+class Review(models.Model):
+    text = models.TextField()
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='rating')
+
---
+++ product/serializers.py
@@ -0,0 +1,41 @@
+from rest_framework import serializers
+from product.models import Category, Product, Review
+
+
+class CategorySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+class CategoryRetrieveSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+
+class ProductSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+class ProductRetrieveSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Review
+        fields = '__all__'
+
+class ReviewRetrieveSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Review
+        fields = '__all__'
